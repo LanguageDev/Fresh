efe14820f756e68475a5af0054194f7ae9ed30d6
Lenkefi Péter <lenkefi.peti@gmail.com>
2022-02-27 15:10:17 +0100

    Continued code builder util

--- Sources/Syntax/Fresh.Syntax.TreeGenerator/CodeBuilder.cs
+++ Sources/Syntax/Fresh.Syntax.TreeGenerator/CodeBuilder.cs
@@ -19,6 +19,7 @@ public enum Modifiers
     Class    = 0b00000001,
     Abstract = 0b00000010,
     Public   = 0b00000100,
+    Override = 0b00001000,
 }
 
 public sealed class CodeBuilder
@@ -53,6 +54,8 @@ public sealed class CodeBuilder
     public CodeBuilder NullableRestore() => this.WriteLine("#nullable restore");
 
     public CodeBuilder Namespace(string ns) => this.WriteLine($"namespace {ns};");
+
+    public CodeBuilder InheritDoc() => this.WriteLine("/// <inheritdoc/>");
 
     public CodeBuilder Doc(string tag, string text, params (string Name, string Value)[] attribs) => this
         .WriteLine($"/// <{tag}{string.Join("", attribs.Select(a => $" {a.Name}=\"{a.Value}\""))}>")
@@ -108,7 +111,78 @@ public sealed class CodeBuilder
 
     public CodeBuilder EndCtor() => this.WriteLine("}");
 
-    private static string EscapeKeyword(string name) => keywords.Contains(name)
-        ? $"@{name}"
-        : name;
+    public CodeBuilder TrivialCtor(Modifiers modifiers, IEnumerable<(string Name, string Type, string Doc)> parameters)
+    {
+        this.StartCtor(modifiers, parameters.Select(p => (ToCamelCase(p.Name), p.Type, p.Doc)));
+        foreach (var (n, _, _) in parameters) this.WriteLine($"this.{n} = {EscapeKeyword(ToCamelCase(n))}");
+        this.EndCtor();
+        return this;
+    }
+
+    public CodeBuilder AutoGetterProp(Modifiers modifiers, string name, string type, string doc) => this
+        .DocSummary(doc)
+        .DumpNonTypeModifiers(modifiers)
+        .WriteLine($"{type} {name} {{ get; }}");
+
+    public CodeBuilder ArrowProp(Modifiers modifiers, string name, string type, string doc, string value) => this
+        .DocSummary(doc)
+        .DumpNonTypeModifiers(modifiers)
+        .WriteLine($"{type} {name} => {value};");
+
+    public CodeBuilder StartGetterProp(Modifiers modifiers, string name, string type, string doc) => this
+        .DocSummary(doc)
+        .DumpNonTypeModifiers(modifiers)
+        .WriteLine($"{type} {name} {{")
+        .WriteLine("get {");
+
+    public CodeBuilder EndGetterProp() => this
+        .WriteLine("}")
+        .WriteLine("}");
+
+    public CodeBuilder Equals(string baseType, IEnumerable<string> props) => this
+        .InheritDoc()
+        .WriteLine($"public override bool Equals(object? other) => this.Equals(other as {baseType});")
+        .InheritDoc()
+        .WriteLine($"public override bool Equals([MaybeNull] {baseType} other) =>")
+        .WriteLine($"other is {baseType} o")
+        .Write(string.Join("", props.Select(p => $"&& object.Equals(this.{p}, o.{p})")))
+        .WriteLine(";");
+
+    public CodeBuilder HashCode(IEnumerable<string> props) => this
+        .InheritDoc()
+        .WriteLine("public override int GetHashCode() => HashCode.Combine(")
+        .WriteLine(string.Join(", ", props.Select(p => $"this.{p}")))
+        .WriteLine(");");
+
+    public CodeBuilder ArrowMethod(
+        string summary,
+        Modifiers modifiers,
+        (string Type, string Doc) ret,
+        string name,
+        IEnumerable<(string Name, string Type, string Doc)> parameters,
+        string? body)
+    {
+        this.DocSummary(summary);
+        foreach (var (n, _, doc) in parameters) this.DocParam(n, doc);
+        this.DocReturns(ret.Doc);
+        this.DumpNonTypeModifiers(modifiers);
+        this.Write($"{ret.Type} {name}(");
+        this.Write(string.Join(", ", parameters.Select(p => $"{p.Type} {p.Name}")));
+        this.Write(")");
+        if(body is not null) this.Write($" => {body}");
+        this.WriteLine(";");
+        return this;
+    }
+
+    private CodeBuilder DumpNonTypeModifiers(Modifiers modifiers)
+    {
+        this.Write(modifiers.HasFlag(Modifiers.Public) ? "public " : "internal ");
+        if (modifiers.HasFlag(Modifiers.Abstract)) this.Write("abstract ");
+        if (modifiers.HasFlag(Modifiers.Override)) this.Write("override ");
+        return this;
+    }
+
+    public static string EscapeKeyword(string name) => keywords.Contains(name) ? $"@{name}" : name;
+
+    public static string ToCamelCase(string name) => $"{char.ToLower(name[0])}{name[1..]}";
 }
